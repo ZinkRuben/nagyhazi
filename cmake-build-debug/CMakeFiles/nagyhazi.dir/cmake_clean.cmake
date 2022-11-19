bea3d4d3cfedffb9c0d6b2a64824866f36c41456
Ruben Zink <zinkruben@protonmail.com>
2022-11-19 12:40:38 +0100

    gauss elimination works for first example

---
+++ cmake-build-debug/CMakeFiles/nagyhazi.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nagyhazi.dir/line_operations.c.obj"
+  "CMakeFiles/nagyhazi.dir/line_operations.c.obj.d"
+  "CMakeFiles/nagyhazi.dir/main.c.obj"
+  "CMakeFiles/nagyhazi.dir/main.c.obj.d"
+  "CMakeFiles/nagyhazi.dir/matrix_io.c.obj"
+  "CMakeFiles/nagyhazi.dir/matrix_io.c.obj.d"
+  "CMakeFiles/nagyhazi.dir/matrix_lib.c.obj"
+  "CMakeFiles/nagyhazi.dir/matrix_lib.c.obj.d"
+  "CMakeFiles/nagyhazi.dir/matrix_operations.c.obj"
+  "CMakeFiles/nagyhazi.dir/matrix_operations.c.obj.d"
+  "CMakeFiles/nagyhazi.dir/value_check.c.obj"
+  "CMakeFiles/nagyhazi.dir/value_check.c.obj.d"
+  "CMakeFiles/nagyhazi.dir/value_checks.c.obj"
+  "CMakeFiles/nagyhazi.dir/value_checks.c.obj.d"
+  "libnagyhazi.dll.a"
+  "nagyhazi.exe"
+  "nagyhazi.exe.manifest"
+  "nagyhazi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/nagyhazi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/nagyhazi.dir/value_checks.c.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/nagyhazi.dir/value_checks.c.obj: \
+ C:\Users\Zear\CLionProjects\nagyhazi_plus\value_checks.c \
+ C:\Users\Zear\CLionProjects\nagyhazi_plus\value_checks.h
